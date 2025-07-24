cf7d8c5abf75aa88215e4cdc3ac44691cf0810f6
ElieteCampos <lilagomes3@hotmail.com>
2025-07-24 18:13:22 -0300

    controller carrinho compra e viewmodel

--- Lanche/Startup.cs
+++ Lanche/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using LanchesMac.Context;
+using LanchesMac.Models;
 using LanchesMac.Repositories;
 using LanchesMac.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
@@ -22,8 +23,13 @@ public class Startup
 
         services.AddTransient<ILancheRepository, LancheRepository>();
         services.AddTransient<ICategoriaRepository, CategoriaRepository>();
+        services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        services.AddScoped(sp => CarrinhoCompra.GetCarrinho(sp));
 
         services.AddControllersWithViews();
+        services.AddMemoryCache();
+        services.AddSession();
+        
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,9 +46,10 @@ public class Startup
             app.UseHsts();
         }
         app.UseHttpsRedirection();
-        app.UseStaticFiles();
 
+        app.UseStaticFiles();
         app.UseRouting();
+        app.UseSession();
 
         app.UseAuthorization();
 
