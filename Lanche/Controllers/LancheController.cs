e4419e2cb96f255d6b1e310271b754debf2f6b37
ElieteCampos <lilagomes3@hotmail.com>
2025-07-29 15:58:23 -0300

    menu Categoria

--- Lanche/Controllers/LancheController.cs
+++ Lanche/Controllers/LancheController.cs
@@ -1,4 +1,5 @@
-﻿using LanchesMac.Repositories.Interfaces;
+﻿using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
 using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,23 +14,39 @@ namespace LanchesMac.Controllers
             _lancheRepository = lancheRepository;
         }
 
-        public IActionResult List()
+        public IActionResult List(string categoria)
         {
-           // ViewData["Titulo"] = "Todos os Lanches";
-           // ViewData["Data"] = DateTime.Now;
-           // var lanches = _lancheRepository.Lanches;
-            //var totalLanches = lanches.Count();
+            IEnumerable<Lanche> lanches;
+            string categoriaAtual = string.Empty;
 
-           // ViewBag.Total = "Total de lanches: ";
-           // ViewBag.TotalLanches = totalLanches;
+            if(string.IsNullOrEmpty(categoria))
+            {
+                lanches = _lancheRepository.Lanches.OrderBy(l => l.LancheId);
+                categoriaAtual = "Todos os lanches";
+            }
+            else
+            {
+                if (string.Equals("Normal", categoria, StringComparison.OrdinalIgnoreCase))
+                {
+                    lanches = _lancheRepository.Lanches
+                        .Where(l => l.Categoria.CategoriaNome.Equals("Normal"))
+                        .OrderBy(l => l.Nome);
+                }
+                else
+                {
+                    lanches = _lancheRepository.Lanches
+                       .Where(l => l.Categoria.CategoriaNome.Equals("Natural"))
+                       .OrderBy(l => l.Nome);
+                }
+                categoriaAtual = categoria;
+            }
+            var lanchesListViewModel = new LancheListViewModel
+            {
+                Lanches = lanches,
+                CategoriaAtual = categoriaAtual,
+            };
 
-            //return View(lanches);
-
-            var lanchesListViewModel = new LancheListViewModel();
-            lanchesListViewModel.Lanches = _lancheRepository.Lanches;
-            lanchesListViewModel.CategoriaAtual = "Categoria atual";
-
-            return View(lanchesListViewModel);
+                return View(lanchesListViewModel);
         }
     }
 }
