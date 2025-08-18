3fed461cb4acf3087953d37fe0d5f07a6f3f9e6e
ElieteCampos <lilagomes3@hotmail.com>
2025-08-18 13:57:03 -0300

    controladores e metodos actions para gerar grafico vendas

---
+++ Lanche/Areas/Admin/Controllers/AdminGraficoController.cs
@@ -0,0 +1,36 @@
+﻿using LanchesMac.Areas.Admin.Servicos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Areas.Admin.Controllers
+{
+
+    public class AdminGraficoController : Controller
+    {    
+        private readonly GraficoVendasService _graficoVendas;
+
+        public AdminGraficoController(GraficoVendasService graficoVendas)
+        {
+            _graficoVendas = graficoVendas;
+        }
+        public JsonResult VendasLanches(int dias)
+        {
+            var lanchesVendasTotais = _graficoVendas.GetVendasLanches(dias);
+            return Json(lanchesVendasTotais);
+        }
+        [HttpGet] //esse calcula as vendas dos 360 dias
+        public IActionResult Index(int dias)
+        {
+            return View();
+        }
+        [HttpGet] //esse calcula as vendas mensais
+        public IActionResult VendasMensal(int dias)
+        {
+            return View();
+        }
+        [HttpGet]// esse calcula vendas semanais
+        public IActionResult VendasSemanal(int dias)
+        {
+            return View();
+        }
+    }
+}
